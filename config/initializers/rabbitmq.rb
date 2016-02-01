b4a22a97c13003d31ee491028dc3dc47e2fc0d7f
Avery Yen <ayen@pivotal.io>
2016-02-01 17:31:06 -0500

    Use p-rabbitmq
    Signed-off-by: Megan Murawski <megan.hyland@emc.com>

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -1,7 +1,7 @@
 $bunny = Bunny.new
 
 if Rails.env == 'production'
-  service = JSON.parse(ENV['VCAP_SERVICES'])["cloudamqp"]
-  rabbit_uri = service.first["credentials"]["uri"]
+  service = JSON.parse(ENV['VCAP_SERVICES'])['p-rabbitmq']
+  rabbit_uri = service.first['credentials']['protocols']['ampq']['uri']
   $bunny = Bunny.new(rabbit_uri)
 end
