0992a7cda737002c26979fb3660c7b42f9d97d31
Megan Murawski <megan.hyland@emc.com>
2016-02-01 17:36:06 -0500

    Spell AMQP correctly
    Signed-off-by: Avery Yen <ayen@pivotal.io>

--- config/initializers/rabbitmq.rb
+++ config/initializers/rabbitmq.rb
@@ -2,6 +2,6 @@ $bunny = Bunny.new
 
 if Rails.env == 'production'
   service = JSON.parse(ENV['VCAP_SERVICES'])['p-rabbitmq']
-  rabbit_uri = service.first['credentials']['protocols']['ampq']['uri']
+  rabbit_uri = service.first['credentials']['protocols']['amqp']['uri']
   $bunny = Bunny.new(rabbit_uri)
 end
